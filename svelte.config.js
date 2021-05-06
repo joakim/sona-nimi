29a0b4a1297d7d964eb664e082795bcbe548e67b
Joakim Stai <joakimstai@gmail.com>
2021-05-07 00:28:16 +0200

    netlify

--- svelte.config.js
+++ svelte.config.js
@@ -1,9 +1,13 @@
 /** @type {import('@sveltejs/kit').Config} */
+
+import adapter from '@sveltejs/adapter-netlify';
+
 const config = {
-	kit: {
-		// hydrate the <div id="svelte"> element in src/app.html
-		target: '#svelte'
-	}
+  kit: {
+    adapter: adapter(),
+    // hydrate the <div id="svelte"> element in src/app.html
+    target: '#svelte'
+  }
 };
 
 export default config;
