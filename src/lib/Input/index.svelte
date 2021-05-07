8b9d5271760061cc90faf590dbb4fd816a3f2ac4
Joakim Stai <joakimstai@gmail.com>
2021-05-07 12:11:35 +0200

    nicer placeholder

--- src/lib/Input/index.svelte
+++ src/lib/Input/index.svelte
@@ -2,14 +2,14 @@
 	import { goto } from '$app/navigation'
 
 	export let query = ''
-	let placeholder = 'toki!\n\nThis tool is for looking up the meaning of words in a Toki Pona text.\n\nClick here to enter or paste some Toki Pona, then press oko.\n\nYou can then move the mouse cursor over the words to see their meanings.'
+	let placeholder = 'toki!  This tool is for looking up the meaning of words in a Toki Pona text.  Click here to enter or paste some Toki Pona, then press oko.  You can then move the mouse cursor over the words to see their meanings.'
 
 	function translate() {
 		goto('/t/?q=' + encodeURIComponent(query))
 	}
 </script>
 
-<textarea class="input" bind:value={query} placeholder={placeholder}></textarea>
+<textarea class="input" bind:value={query} placeholder={placeholder.replace(/  /g, "\n\n")}></textarea>
 
 <button class="btn" on:click={translate} disabled={query.length === 0}>o lukin</button>
 
